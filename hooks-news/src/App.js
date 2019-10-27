94b6716fc2744466e30063b1b17ebeca990fece8
Junior Dugue <jrdugue@gmail.com>
2019-10-26 23:59:27 -0400

    incoporated the useEffect and imported axios to app comp

---
+++ hooks-news/src/App.js
@@ -0,0 +1,13 @@
+import React, {useEffect} from 'react';
+import axios from 'axios';
+
+export default function App(){
+  useEffect(() => {
+    axios.get('')
+  })
+  return (
+    <div>
+      <h1>App</h1>
+    </div>
+  );
+}
